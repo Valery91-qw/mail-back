d9e9b411b6b8fcfe1787c5c55d7d8ad410391c9a
Valery91-qw <vakriv91@gmail.com>
2021-03-01 21:00:45 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const express = require( "express" );
+const app = express();
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const nodemailer = require("nodemailer");// import nodemailer
+const port = 3009; // default port to listen
+
+
+let mail = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "vakriv91@gmail.com", // generated ethereal user
+        pass: "Glavryba139", // generated ethereal password
+    },
+});
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// define a route handler for the default home page
+app.get( "/", ( req, res ) => {
+    res.send( "Hello world!" );
+} );
+
+app.post( "/sendMessage", async function( req, res ) {
+
+    let {name, email, message} = req.body
+    let info = await mail.sendMail({
+        from: 'my profile page',
+        to: 'vakriv91@gmail.com',
+        subject: 'Sending Email via Node.js',
+        html: `<div>name : ${name}</div>
+<div>email: ${email}</div>
+<div>message: ${message}</div>`
+    });
+
+    res.send(req.body);
+});
+
+// start the Express server
+app.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` );
+} );

d0f84beb6703db79253debbf5dd2a95ecce3eb25
Valery91-qw <vakriv91@gmail.com>
2021-03-06 15:34:18 +0300

    NodeMailer

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ let port = process.env.PORT || 3009
 
 app.use(cors());
 const bodyParser = require('body-parser')
-const nodemailer = require("nodemailer");// import nodemailer
+const nodemailer = require("nodemailer");
+const nodeMailer = require("nodemailer");
+// import nodemailer
 let allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
@@ -28,7 +30,7 @@ app.use(bodyParser.json())
 app.use(allowCrossDomain);
 
 
-let mail = nodemailer.createTransport({
+let mail = nodeMailer.createTransport({
     service: "gmail",
     auth: {
         type: "login",
